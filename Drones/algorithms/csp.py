783695d9721671a1ca6bf8eda189f8fc7ac978bc
Lucas Ruiz <l.ruiz11@uniandes.edu.co>
2026-03-10 08:43:36 -0500

    backtraking

--- Drones/algorithms/csp.py
+++ Drones/algorithms/csp.py
@@ -24,8 +24,25 @@ def backtracking_search(csp: DroneAssignmentCSP) -> dict[str, str] | None:
     You can find inspiration in the textbook's pseudocode:
     Artificial Intelligence: A Modern Approach (4th Edition) by Russell and Norvig, Chapter 5: Constraint Satisfaction Problems
     """
+
     # TODO: Implement your code here
+
+
+    return backtracking(csp, {})
+
+def backtracking(csp: DroneAssignmentCSP, assignment):
+    if csp.is_complete(assignment):
+        return assignment
+    var = csp.get_unassigned_variables(assignment=assignment)[0]
+    for value in csp.domains[var]:
+        if csp.is_consistent(var,value,assignment):
+            csp.assign(var,value,assignment)
+            result = backtracking(csp,assignment)
+            if result:
+                return result
+            csp.unassign(var,assignment)
     return None
+        
 
 
 def backtracking_fc(csp: DroneAssignmentCSP) -> dict[str, str] | None:
@@ -61,6 +78,7 @@ def backtracking_ac3(csp: DroneAssignmentCSP) -> dict[str, str] | None:
       - a backtrack function that integrates AC-3 into the search process.
     """
     # TODO: Implement your code here
+
     return None
 
 
